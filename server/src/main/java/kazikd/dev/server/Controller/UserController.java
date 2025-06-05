2d6acac1f52f8546c89f0436bb35bb1de9698df0
Michał Kaźmierczak <kmiki246@gmail.com>
2025-06-05 20:28:18 +0200

    fix: enable CORS

--- server/src/main/java/kazikd/dev/server/Controller/UserController.java
+++ server/src/main/java/kazikd/dev/server/Controller/UserController.java
@@ -3,12 +3,14 @@ package kazikd.dev.server.Controller;
 import kazikd.dev.server.Model.CityDTO;
 import kazikd.dev.server.Service.CitiesService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class UserController {
 
